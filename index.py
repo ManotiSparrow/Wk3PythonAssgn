2dddf24fc713c51cffa374e34d9f1f2cf51b98c3
SparrowM <nyamoitasparrow@gmail.com>
2025-03-31 10:47:20 +0300

    added print

--- index.py
+++ index.py
@@ -8,3 +8,5 @@ price = float(input("Enter the original price of the item: "))
 discount_percent = float(input("Enter the discount percentage: "))
 
 final_price = calculate_discount(price, discount_percent) 
+
+print(final_price)
